83a22680a2d53f8014f3636bfc8a89a4b82b3ce1
Joachim Happel <joachim.happel@gmail.com>
2024-03-13 16:46:38 +0100

    fixes

--- build/block/index.asset.php
+++ build/block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8d4aa0c5009786aa3c6a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd87f6df84b46c7f503da');
